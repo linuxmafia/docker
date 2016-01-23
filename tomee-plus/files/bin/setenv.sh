ebf037b35be724d0c9ce4cd50c8b68376208ee7a
Super Wang <wangxuepo@gmail.com>
2016-01-23 15:45:35 -0500

    fix quoted mark

--- tomee-plus/files/bin/setenv.sh
+++ tomee-plus/files/bin/setenv.sh
@@ -3,14 +3,13 @@
 export CATALINA_OPTS="$CATALINA_OPTS -server"
 export CATALINA_OPTS="$CATALINA_OPTS -Xms512m"
 export CATALINA_OPTS="$CATALINA_OPTS -Xmx2048m"
-export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxPermSize=256m"
 
 if [ -z "$SERVER_ID" ]; then
-	export SERVER_ID=server
+	export SERVER_ID="server"
 fi
 
 if [ -z "$SERVER_HOSTNAME" ]; then
-	export SERVER_HOSTNAME=localhost
+	export SERVER_HOSTNAME="localhost"
 fi
 
-export JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8" -Dserver.id=$SERVER_ID -Dserver.hostname=$SERVER_HOSTNAME"
+export JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dserver.id=$SERVER_ID -Dserver.hostname=$SERVER_HOSTNAME"
